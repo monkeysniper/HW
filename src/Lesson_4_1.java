fa0a8fef5d43821c7fe092b5af04a41c5c0b5684
kairatabdillaev <kajratabdillaev@gmail.com>
2025-01-15 21:16:04 +0600

    check1

---
+++ src/Lesson_4_1.java
@@ -0,0 +1,126 @@
+import java.util.Random;
+
+public class Lesson_4_1 {
+    public static int bossHealth = 700;
+    public static int bossDamage = 50;
+    public static String bossDefence;
+    public static String[] heroesAttackType = {"Physical", "Magical", "Kinetic","Medic",};
+    public static int[] heroesHealth = {280, 270, 250,200};
+    public static int[] heroesDamage = {20, 10, 15, 0};
+    public static int roundNumber;
+
+    public static void main(String[] args) {
+        printStatistics();
+
+        while (!isGameOver()) {
+            playRound();
+        }
+    }
+
+    public static boolean isGameOver() {
+        if (bossHealth <= 0) {
+            System.out.println("Heroes won!!!");
+            return true;
+        }
+        boolean allHeroesDead = true;
+        for (int currentHP : heroesHealth) {
+            if (currentHP > 0) {
+                allHeroesDead = false;
+                break;
+            }
+        }
+        if (allHeroesDead) {
+            System.out.println("Boss won!!!");
+            return true;
+        }
+        return false;
+    }
+
+    public static void playRound() {
+        roundNumber++;
+        chooseBossDefence();
+        bossAttack();
+        heroesAttack();
+        medicheal1();
+        printStatistics();
+    }
+
+    public static void chooseBossDefence() {
+        Random random = new Random();
+        int randomIndex = random.nextInt(heroesAttackType.length); // 0,1,2
+        bossDefence = heroesAttackType[randomIndex];
+    }
+
+    public static void heroesAttack() {
+        for (int i = 0; i < heroesDamage.length; i++) {
+            if (heroesHealth[i] > 0 && bossHealth > 0) {
+                int damage = heroesDamage[i];
+                if (heroesAttackType[i] == bossDefence) {
+                    Random random = new Random();
+                    int coeff = random.nextInt(2,10); // 2,3,4,5,6,7,8,9
+                    damage = heroesDamage[i] * coeff;
+                    System.out.println("Critical damage: " + damage + " (" + coeff + ")");
+                }
+                if (bossHealth - damage < 0) {
+                    bossHealth = 0;
+                } else {
+                    bossHealth = bossHealth - damage;
+                }
+            }
+        }
+    }
+public static void medicheal1(){
+    for (int i = 0; i <heroesHealth.length ; i++) {
+        if(heroesHealth[i] <= 100){
+            Random random= new Random();
+            int heal = random.nextInt(150);
+            if(heroesHealth[3] <= 0){
+                heal = 0;
+            }
+            if (heroesAttackType[i]==heroesAttackType[3]){
+                    continue;
+            }
+            if (heroesHealth[i]>=0){
+                heroesHealth[i] += heal;
+                System.out.println("Heal = " + heal);
+                break;
+            }
+        }
+    }
+}
+
+    public static void bossAttack() {
+        for (int i = 0; i < heroesHealth.length; i++) {
+            if (heroesHealth[i] > 0) {
+                if (heroesHealth[i] - bossDamage < 0) {
+                    heroesHealth[i] = 0;
+                } else {
+                    heroesHealth[i] = heroesHealth[i] - bossDamage;
+                }
+            }
+        }
+    }
+
+    public static void printStatistics() {
+        System.out.println("ROUND: " + roundNumber + " -----------------");
+        /*String defence;
+        if (bossDefence == null) {
+            defence = "No defence";
+        } else {
+            defence = bossDefence;
+        }*/
+        System.out.println("Boss health: " + bossHealth + " damage: " + bossDamage +
+                " defence: " + (bossDefence == null ? "No defence" : bossDefence));
+        for (int i = 0; i < heroesHealth.length; i++) {
+            System.out.println(heroesAttackType[i] + " health: " + heroesHealth[i] +
+                    " damage: " + heroesDamage[i]);
+        }
+    }
+}
+
+
+//**Добавить n-го игрока, Golem, который имеет увеличенную жизнь но слабый удар. Принимает на себя 1/5 часть урона исходящего от босса по другим игрокам.
+//        **Добавить n-го игрока, Lucky, имеет шанс 25% уклонения от ударов босса.
+//        **Добавить еще игрока Witcher, не наносит урон боссу, но получает урон от босса. Имеет 1 шанс оживить первого погибшего героя, отдав ему свою текущую жизнь, при этом погибает сам.
+//        *Добавить n-го игрока, Thor, удар по боссу имеет шанс 50% оглушить босса на 1 раунд, вследствие чего босс пропускает 1 раунд и не наносит урон героям.
+// random.nextBoolean(); - true, false
